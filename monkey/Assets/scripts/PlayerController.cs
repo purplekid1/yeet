b0aa06b920bc443889a3625d4ff6ab1a4a1f3920
purplekid1 <71616134+purplekid1@users.noreply.github.com>
2024-09-09 10:23:46 -0700

    created test level with player

---
+++ monkey/Assets/scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody myRB;
+
+    public float speed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            myRB.AddForce(transform.forward * speed);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            myRB.AddForce(transform.forward * -speed);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRB.AddForce(transform.right * speed);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRB.AddForce(transform.forward * -speed);
+
+        }
+
+    }
+}
