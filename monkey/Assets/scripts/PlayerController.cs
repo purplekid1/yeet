0792edca42130b8f1562cebeb5b45d7f60051e6f
purplekid1 <71616134+purplekid1@users.noreply.github.com>
2024-09-10 10:08:11 -0700

    updated player movement and added camera controls

--- monkey/Assets/scripts/PlayerController.cs
+++ monkey/Assets/scripts/PlayerController.cs
@@ -5,40 +5,52 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody myRB;
+    Camera playerCam;
+
+    Vector2 camRotation;
 
     public float speed = 10f;
+    public float jumpHeight = 5f;
+
+    public float mouseSensitivity = 2.0f;
+    public float Xsensitivity = 2.0f;
+    public float Ysensitivity = 2.0f;
+
+    public float camRotationLimit = 90f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
+        playerCam = transform.GetChild(0).GetComponent<Camera>();
+
+        camRotation = Vector3.zero;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            myRB.AddForce(transform.forward * speed);
 
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            myRB.AddForce(transform.forward * -speed);
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            myRB.AddForce(transform.right * speed);
+        playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
 
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            myRB.AddForce(transform.forward * -speed);
+        Vector3 temp = myRB.velocity;
 
+        temp.z = Input.GetAxisRaw("Horizontal") * speed;
+        temp.x = Input.GetAxisRaw("Vertical") * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            temp.y = jumpHeight;
         }
 
+        myRB.velocity = (transform.forward * temp.x) + (transform.right * temp.z) + (transform.up * temp.y);
     }
 }
